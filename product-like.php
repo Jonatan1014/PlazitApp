057d796d42e8f3d438ac7a6ed4794caa9a2db31c
Jonatan Cantillo <lamotomasrrapidadelmundo@gmail.com>
2024-11-25 15:05:55 -0500

    elmnacion de mensaje de añadido a favorito funcionando

--- product-like.php
+++ product-like.php
@@ -72,7 +72,7 @@ $productos_carrito = $carrito->listar_productos_carrito($user['usuario_id']);
                                           <h5 class="fs-6 mb-0"><a href="#" class="text-inherit"><?php echo htmlspecialchars($producto['nombre']); ?></a></h5>
                                        </div>
                                     </td>
-                                    <td><?php echo htmlspecialchars($producto['descripcion'] .$producto['producto_id']); ?></td>
+                                    <td><?php echo htmlspecialchars($producto['descripcion']); ?></td>
                                     <td>$<?php echo number_format($producto['precio'], 2); ?> </td>
                                     <td>
                                        <?php echo $producto['stock'] > 0 ? '<span class="badge bg-success">En stock</span>' : '<span class="badge bg-danger">Agotado</span>'; ?>
@@ -81,9 +81,9 @@ $productos_carrito = $carrito->listar_productos_carrito($user['usuario_id']);
                                        
 
                                     <form action="action/product-add-car.php" method="POST">
-    <input type="hidden" name="producto_id" value="<?php echo htmlspecialchars($producto['producto_id']); ?>">
-    <button type="submit" class="btn btn-primary btn-sm">Añadir al Carrito</button>
-</form>
+                                       <input type="hidden" name="producto_id" value="<?php echo htmlspecialchars($producto['producto_id']); ?>">
+                                       <button type="submit" class="btn btn-primary btn-sm">Añadir al Carrito</button>
+                                    </form>
 
 
                                     </td>
